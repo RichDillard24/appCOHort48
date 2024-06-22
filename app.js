ee9aa867f25aa1ab39581e8371419057a1306da5
RichDillard24 <richiegd@hotmail.com>
2024-06-21 17:15:32 -0700

    final

--- app.js
+++ app.js
@@ -29,7 +29,20 @@ function saveTask(){
         }
     });
 
-    
+}
+function deleteTask(id){
+    console.log("Delete task btn works", id)
+
+    $.ajax({
+        type: "DELETE",
+        url: `http://fsdiapi.azurewebsites.net/api/tasks/${id}/`,
+        success: function () {
+            $("#" + id).remove();
+        },
+        error: function (error) {
+            console.log("Error:", error);
+        },
+    })
 }
 function loadTask(){
     $.ajax({
@@ -56,15 +69,19 @@ function loadTask(){
 }
 
 function displayTask(task){
-    let syntax =` <div class='task'>
-    <div class='info'>
-        <h3>${task.title}</h3>
-        <h5>${task.description}</h5>
-    </div>
-        <label class="status">${task.status}</label>
-        <div class='date-budget">
-        <label>${task.date}</label>
-        <label>${task.budget}</label>
+    let syntax =` 
+    <div class='task' id="${task._id}">
+        <div class='info'>
+            <h3>${task.title}</h3>
+            <h5>${task.description}</h5>
+        </div>
+        <label class='status'>${task.status}</label>
+        <div class='date-budget'>
+            <label>${task.date}</label>
+            <label>${task.budget}</label>
+        </div>
+        <div class="mb-3" >
+            <button class="form-label" type="button" class="btn btn-secondary" id="btnDelete" onClick="deleteTask('${task._id}')">Delete</button>
         </div>
     </div>
     `
@@ -88,5 +105,6 @@ function init(){
     loadTask();
 
     $("#btnSave").click(saveTask);
+    // $("#btnDelete").click(deleteTask);
 }
 window.onload  = init;
